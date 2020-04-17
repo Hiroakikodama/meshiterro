1588e36c8ffbfe598dcf93d8c4e80c8291fb74f4
KodamaHiroaki <kdm@KodamaHiroakinoMacBook-Air.local>
2020-04-17 12:49:38 +0900

    Finish5-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show] do
+  	# resourcesなのでそのコントローラのidがリクエストに含まれる。
+  	resource :favorites, only: [:create, :destroy]
+  	resource :post_comments, only: [:create]
+  	# resourceにするとそのコントローラのidがリクエストに含まれなくなる。（＝詳細showページの表示が不要ならrecource）
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
